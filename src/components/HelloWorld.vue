6c0967e8d5877d9ab692fd49e9c7f9625ea9b6cb
Maria Isabelle Te <miclavelte@gmail.com>
2019-09-19 20:19:50 -0700

    use prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
@@ -97,17 +97,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
+h3 {
+  margin: 40px 0 0;
+}
 
-ul
-  list-style-type none
-  padding 0
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-li
-  display inline-block
-  margin 0 10px
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-a
-  color #42b983
+a {
+  color: #42b983;
+}
 </style>
