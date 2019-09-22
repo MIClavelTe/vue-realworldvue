50ee8917e25be2f19777572e6864c1ba7a4b4119
Maria Isabelle Te <miclavelte@gmail.com>
2019-09-22 13:31:21 -0700

    create an add card form

---
+++ src/components/addCardForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <p>
+      <label>Question:</label>
+      <input v-model="question" />
+    </p>
+
+    <p>
+      <label>Answer:</label>
+      <input v-model="answer" />
+    </p>
+
+    <p>
+      <input type="submit" value="Submit" />
+    </p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      question: "",
+      answer: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      let card = {
+        question: this.question,
+        answer: this.answer
+      };
+      this.$emit("card-submitted", card);
+      this.question = null;
+      this.answer = null;
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,25 @@
       </li>
     </ol>
 
-    <p>Remove Flash Card #:</p>
-    <input type="text" v-model="deletedCard" />
-    <button @click="deleteCard">Delete</button>
+    <!-- Only show remove div if more than 0 cards -->
+    <div v-if="cards.length > 0">
+      <p>Remove Flash Card #:</p>
+      <input type="text" v-model="deletedCard" />
+      <button @click="deleteCard">Delete</button>
+    </div>
+
+    <h2>Add Flash Card</h2>
+    <AddCardForm @card-submitted="addCard" />
   </div>
 </template>
 
 <script>
 import Card from "../components/card";
+import AddCardForm from "../components/addCardForm";
 export default {
   components: {
-    Card
+    Card,
+    AddCardForm
   },
   data() {
     return {
@@ -42,6 +50,9 @@ export default {
     deleteCard() {
       var start = this.deletedCard - 1;
       this.cards.splice(start, 1);
+    },
+    addCard(card) {
+      this.cards.push(card);
     }
   }
 };
